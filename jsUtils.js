22e8bdde19d613aa049332034531c208cd0d5cde
akovylyaeva <akovylyaeva@tourmalinecore.com>
2024-05-07 15:26:05 +0500

    chore: setting up custom header

---
+++ jsUtils.js
@@ -0,0 +1,44 @@
+function fn() {
+    return {
+        getAuthHeaders: function (tokenValue) {
+            return {
+                [this.getAuthHeaderKey()]: this.getAuthHeaderValue(tokenValue)
+            }
+        },
+
+        getAuthHeaderValue: function (tokenValue) {
+            return this.shouldUseFakeExternalDependencies()
+                ? tokenValue
+                : 'Bearer ' + tokenValue;
+        },
+
+        getAuthHeaderKey: function () {
+            return this.shouldUseFakeExternalDependencies()
+                ? 'X-DEBUG-TOKEN'
+                : 'Authorization';
+        },
+
+        shouldUseFakeExternalDependencies: function () {
+            var System = Java.type('java.lang.System');
+
+            return System.getenv('SHOULD_USE_FAKE_EXTERNAL_DEPENDENCIES') === 'true';
+        },
+
+        command: function (line) {
+            var proc = karate.fork({ redirectErrorStream: false, line: line });
+
+            // function listener(line) {
+            //     if (line.contains('file sent')) {
+            //       karate.log('stopping tool');      
+            //       karate.signal(proc.sysOut);
+            //     }
+            // }
+
+            // karate.fork(line);
+            // proc.waitSync();
+            karate.set('sysOut', proc.sysOut);
+            karate.set('sysErr', proc.sysErr);
+            // karate.set('exitCode', proc.exitCode);
+        },
+    }
+}
