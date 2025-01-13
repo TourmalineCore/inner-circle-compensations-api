afc1c0a217a712f4d6a46d47064276360284f116
Anna <141008173+Annushkaaaaa@users.noreply.github.com>
2025-01-13 02:11:45 -0800

    feat: add hard-delete endpoint for compensation (#22)
    * feat: add hard-delete endpoint for compensation
    
    * chore: change permission of endpoint

---
+++ Application/Commands/CompensationHardDeletionCommand.cs
@@ -0,0 +1,26 @@
+using Application.Dtos;
+using Core;
+using DataAccess;
+
+namespace Application.Commands;
+
+public class CompensationHardDeletionCommand
+{
+    private readonly CompensationsDbContext _context;
+
+    public CompensationHardDeletionCommand(CompensationsDbContext сompensationsDbContext)
+    {
+        _context = сompensationsDbContext;
+    }
+
+    public async Task ExecuteAsync(long id)
+    {
+        var compensation = _context.Compensations.SingleOrDefault(x => x.Id == id);
+
+        if (compensation != null)
+        {
+            _context.Remove(compensation);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
